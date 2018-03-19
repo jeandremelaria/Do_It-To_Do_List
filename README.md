fe4c92645e289e2f0aabac2f433b85523f96d0a2
Jeandre Melaria <J.melaria@hotmail.com>
2018-03-19 21:05:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Title : ToDo List 
+
+## Version : 1.0.0 (Change log) (CHANGELOG.md)
+
+## Description : A simple to do list made with HTML5, CSS3 and Materialize css. 
+
+## Installation instructions : 
+	- Clone repository
+	- Run npm install to install devDependencies.
+	- Edit files in src folder.
+	- Deploy or upload files in the build folder.
+
+## Contributors
+Jeandre Melaria – j.melaria@hotmail.com
+
+## License & copyright
+
+&copy; Jeandre Melaria 
+
+Licensed under the (MIT License)(LICENSE).
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+//---- CONSTRANTS ----//
+const gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    htmlmin = require('gulp-htmlmin');
+
+//---- LOG MESSAGE ----//
+gulp.task('message', function() {
+    return console.log('Gulp is running...');
+});
+
+//---- GULP PREFIXER AND CSS NANO ----//
+gulp.task('prefixerAndCssNano', function() {
+    return gulp.src('src/css/*.css')
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cssnano())
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('build/css'));
+});
+
+//---- MINIFY JS ----//
+gulp.task('minifyJs', function(cb) {
+    pump([
+            gulp.src('src/js/*.js'),
+            uglify(),
+            concat('main.js'),
+            gulp.dest('build/js')
+        ],
+        cb
+    );
+});
+
+//---- COPY ALL HTML FILES TO BUILD FOLDER AND MINIFY ----//
+gulp.task('copyHtml', function() {
+    gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'));
+
+    gulp.src('src/includes/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build/includes'));
+});
+
+//---- RUN MULTIPLE TASKS AT ONCE IN ARRAY ----//
+gulp.task('default', ['message', 'prefixerAndCssNano', 'minifyJs', 'copyHtml']);
+
+//---- GULP WATCH ----//js
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['minifyJs']);
+    gulp.watch('src/css/*.css', ['prefixerAndCssNano']);
+    gulp.watch('src/includes/*.html', ['copyHtml']);
+    gulp.watch('src/*.html', ['copyHtml']);
+
+});
